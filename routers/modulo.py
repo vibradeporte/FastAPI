fc0d46d31f628578f59f88cd72397ab9adcad741
vibradeporte <info@vibradeporte.com>
2024-02-28 17:01:01 -0500

    Update "modulo"

--- routers/modulo.py
+++ routers/modulo.py
@@ -25,29 +25,28 @@ modulo_router = APIRouter()
 def modulos_curso(id: str = Query(max_length=max_lenght_username), curso: str = Query(max_length=max_lenght_courseshortname)):
     with engine.connect() as connection:
         consulta_sql = text(f"""
-            SELECT  subconsulta.quiz_id as quiz_id, subconsulta.quiz_name as quiz_name,
-                    subconsulta.attempt_count as quiz_attempt_count, subconsulta.Progress as quiz_status
+            SELECT  subconsulta.quiz_id as quiz_id, subconsulta.quiz_name as quiz_name, subconsulta.attempt_count as quiz_attempt_count,
+                    subconsulta.Progress as quiz_status
             FROM    (SELECT q.id AS quiz_id, q.name AS quiz_name, COUNT(qa.id) AS attempt_count,
                     CASE    WHEN cmc.completionstate = 0 THEN 'In Progress'
                             WHEN cmc.completionstate = 1 THEN 'Completed'
                             WHEN cmc.completionstate = 2 THEN 'Completed with Pass'
                             WHEN cmc.completionstate = 3 THEN 'Completed with Fail'
-                            ELSE 'Unknown'
-                            END AS 'Progress'
-                    FROM    mdl_quiz q
-                    JOIN    mdl_course c ON (q.course = c.id) AND (c.shortname = :curso)
-                    JOIN    mdl_quiz_attempts qa ON qa.quiz = q.id
-                    LEFT JOIN   mdl_user u ON (qa.userid = u.id) AND (u.username = :id)
-                    JOIN    mdl_course_modules_completion cmc ON cmc.userid = u.id
-                    JOIN    mdl_course_modules cm ON (cmc.coursemoduleid = cm.id) AND (cm.course = c.id) AND (q.id = cm.instance)
-                    JOIN    mdl_modules m ON cm.module = m.id
-                    GROUP BY q.name) as subconsulta
-            WHERE   (subconsulta.attempt_count>=4) AND (subconsulta.Progress='Completed with Fail');
-        """).params(curso=curso, id=id)
+                    ELSE 'Desconocido'
+                    END AS 'Progress'
+                    FROM mdl_quiz q JOIN mdl_course c ON (q.course = c.id) AND (c.shortname = :curso)
+                    JOIN mdl_quiz_attempts qa ON qa.quiz = q.id
+                    JOIN mdl_user u ON (qa.userid = u.id) AND (u.username = :id)
+                    JOIN mdl_course_modules cm ON (cm.course = c.id) AND (q.id = cm.instance)
+                    JOIN mdl_modules m ON cm.module = m.id
+                    LEFT JOIN mdl_course_modules_completion cmc ON (cmc.userid = u.id) AND (cmc.coursemoduleid = cm.id)
+                    GROUP BY q.id) as subconsulta
+            WHERE   (subconsulta.attempt_count>=4) AND ((subconsulta.Progress='Completed with Fail') OR (subconsulta.Progress='Desconocido'));
+            """).params(curso=curso, id=id)
         result = connection.execute(consulta_sql)
         rows = result.fetchall()
         if rows:
             module_data = [{'quiz_name': row[1], 'status': row[3]} for row in rows]
             return JSONResponse(status_code=200, content={'modulos': module_data, 'message': "Los modulos del curso han sido encontrados."})
         else:
-            return JSONResponse(status_code=404, content={'message': "No existen modulos para el curso consultado"})
+            return JSONResponse(status_code=404, content={'message': "No existen modulos perdidos para el curso consultado"})
